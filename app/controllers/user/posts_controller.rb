0db7aa4f2659309c679a4abc3332c1d1b88d270f
shinju0514 <shinju0514yellow@gmail.com>
2022-05-11 05:40:05 +0000

    user_post

--- app/controllers/user/posts_controller.rb
+++ app/controllers/user/posts_controller.rb
@@ -3,6 +3,7 @@ class User::PostsController < ApplicationController
   end
 
   def show
+
   end
 
   def edit
@@ -12,11 +13,25 @@ class User::PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "投稿しました"
+      redirect_to post_path(@post.id)
+    else
+      render :new
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:menu, :post_image, :body)
   end
 end
