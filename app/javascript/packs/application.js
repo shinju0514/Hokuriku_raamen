0a08ca8844845134d9141a1ec553b79aa8b894cc
shinju0514 <shinju0514yellow@gmail.com>
2022-05-13 07:33:44 +0000

    rate_post

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,13 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
 import "jquery";
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"
+window.$ = window.jQuery = require('jquery');
+require('packs/raty')
+
 
 
 Rails.start()
