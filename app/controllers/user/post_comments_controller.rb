54bb5b03bf493ffdb49baf827dff0e411d34fa3a
shinju0514 <shinju0514yellow@gmail.com>
2022-05-14 15:58:23 +0000

    javascript_favo_comment

--- app/controllers/user/post_comments_controller.rb
+++ app/controllers/user/post_comments_controller.rb
@@ -1,15 +1,14 @@
 class User::PostCommentsController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.post_id = post.id
-    comment.save
-    redirect_to post_path(post.id)
+    @post = Post.find(params[:post_id])
+    @comment = current_user.post_comments.new(post_comment_params)
+    @comment.post_id = @post.id
+    @comment.save
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
     PostComment.find(params[:id]).destroy
-    redirect_to post_path(params[:post_id])
   end
 
   private
