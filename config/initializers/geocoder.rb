be379f5e0b6d139c91ac23fb8cc29fb4ae637840
shinju0514 <shinju0514yellow@gmail.com>
2022-08-04 03:12:54 +0000

    google map

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -8,7 +8,7 @@ Geocoder.configure(
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  api_key: 'AIzaSyBJtlGNe5CprkjNaTdjo1gQKPc0DwFTvmI',               # API key for geocoding service
+  api_key: ENV['API_KEY'],               # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
 
   # Exceptions that should not be rescued by default
